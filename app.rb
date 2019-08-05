fca579fb88f5fae5bc87bcdd8a3295aa39d17250
Courtney Dyson <courtneydyson87@gmail.com>
2019-08-05 09:10:11 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is Courtney"
+    end
+    get '/hometown' do
+        "My hometown is Stone Mountain, GA."
+    end
+    get '/favorite-song' do
+        "My favorite song is Before I Let Go."
+    end
 end
